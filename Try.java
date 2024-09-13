b8a10e8fa1b51cdf08660bfe97006bb761853f56
Keshav Pathak <keshavrp2001@gmail.com>
2024-09-13 11:58:51 +0530

    Update Try.java

--- Try.java
+++ Try.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Try {
     public static void main(String[] args) {
         int []arr={9,8,6,3,11,12};
-        for(int i=1;i< arr.length;i++){
+        for(int i=1;i< arr.length-1;i++){
             int k=i;
                 while(k>0 && arr[k]<arr[-1]){               int temp=arr[k];
                 arr[k]=arr[k-1];

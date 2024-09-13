233443f54f31b44163b9f575d2d832369717c02b
Keshav Pathak <keshavrp2001@gmail.com>
2024-09-13 11:58:15 +0530

    Update Try.java

--- Try.java
+++ Try.java
@@ -6,14 +6,12 @@ public class Try {
     public static void main(String[] args) {
         int []arr={9,8,6,3,11,12};
         for(int i=1;i< arr.length;i++){
-            int index=i;
             int k=i;
-                while(arr[k]<arr[k-1] && k>0){
-                    index=k;
-                    k--;
+                while(k>0 && arr[k]<arr[-1]){               int temp=arr[k];
+                arr[k]=arr[k-1];
+                arr[k-1]=temp;
+                k--;
                 }
-                for(int l=k-1;k>)
-
 
         }
         for(int item:arr){
